ff1bdc8ee1ecfc3f796fac5b3a6e334256bbfa60
Marcos Souza <marcos.souza@beeleads.com.br>
2022-09-28 19:39:58 -0300

    Creating middlaware for deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient';
+import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateDeliveryman';
 import { AuthenticateClientController } from './modules/account/AuthenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/AuthenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/CreateDelivery/CreateDeliveryController';
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/FindAllAvailableController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
@@ -10,10 +14,17 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableController = new FindAllAvailableController();
 
 routes.post('/client/', createClientController.handle);
 routes.post('/client/authenticate/', authenticateClientController.handle);
+
 routes.post('/deliveryman/', createDeliverymanController.handle);
 routes.post('/deliveryman/authenticate/', authenticateDeliverymanController.handle);
+
+routes.post('/delivery', ensureAuthenticateClient, createDeliveryController.handle);
+routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllAvailableController.handle);
+
 
 export { routes }
